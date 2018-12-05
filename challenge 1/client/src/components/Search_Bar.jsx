c084f47a7ec6de276a1ebfc6cb58d7957f9636d2
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-05 00:07:55 -0800

    flesh out boilerplate code for Search_Bar

--- challenge 1/client/src/components/Search_Bar.jsx
+++ challenge 1/client/src/components/Search_Bar.jsx
@@ -6,5 +6,18 @@ export class Search extends Component {
     this.state = {
       query: '',
     };
+    this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this);
+  }
+
+  handleSearchQueryChange(event) {
+    this.setState({query: event.target.value})
+  }
+
+  render() {
+    return (
+      <input
+        onChange={(event) => this.handleSearchQueryChange}
+        placeholder={'Search...'}/>
+    );
   }
 }
