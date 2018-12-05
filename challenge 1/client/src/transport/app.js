ca226e06a1366df490eff8874fd0ece3965ed1d3
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-05 00:02:54 -0800

    outlined all endpoints for app

--- challenge 1/client/src/transport/app.js
+++ challenge 1/client/src/transport/app.js
@@ -1,4 +1,8 @@
 
 // Endpoints
 // GET `http://localhost:3000/books?_page=${nextPageId}` // paginates by 10 records
+// GET `http://localhost:3000/posts?q=${query}` // searches by query
 // GET `http://localhost:3000/posts?q=${query}&_page=${nextPageId}` // searches by query and paginates by 10 records
+// PUT `http://localhost:3000/books?_page=${nextPageId}` // paginates by 10 records
+// PUT `http://localhost:3000/posts?q=${query}` // searches by query
+// PUT `http://localhost:3000/posts?q=${query}&_page=${nextPageId}` // searches by query and paginates by 10 records
