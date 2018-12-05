c7f589f11a83decf538405719d403e69dcfacc2c
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-04 23:39:55 -0800

    make progress on setting up boilerplate code

---
+++ challenge 1/client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactPaginate from 'react-paginate';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: []
+    }
+  }
+}
