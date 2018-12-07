ac18777807596962d1c11f7e3cf54cd3f1a60603
IsaacLeeWebDev <isaacleewebdev@gmail.com>
2018-12-06 18:46:34 -0800

    set up boilerplate code

---
+++ challenge_3/TODO.md
@@ -0,0 +1,4 @@
+# TODO for Mini-apps II-III:
+
+- [ ] Set up boilerplate code
+  - [ ] get hello-world
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 8888;
 
+app.use(express.static('public'));
+
+app.listen(port, `listening on ${port}\nV^V^V^V^V^V^V\n\n`);
 
